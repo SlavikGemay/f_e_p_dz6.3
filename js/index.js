c741e696027c314519613df49c30c1ac67e81fd3
SlavikGemay <gemayit@gmail.com>
2025-02-04 21:10:03 +0200

    dz6.3 update

--- js/index.js
+++ js/index.js
@@ -15,9 +15,8 @@ function removeElement() {
     console.log(array);
     // return array;
 }
+removeElement();
 
-
-// console.log(removeElement);
 
 
 // const array = [1, 3, 4, 4, 6, 2, 5, 4, 7];
