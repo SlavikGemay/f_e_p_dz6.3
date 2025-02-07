c216bb80e32fe4963868d523eece805f710268db
SlavikGemay <gemayit@gmail.com>
2025-02-07 13:16:53 +0200

    dz6.3 update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const array = [1, 3, 4, 4, 6, 2, 5, 4, 7];
-function removeElement() {
 
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === 4) {
-            array.splice(i, 1);
+function removeElement(arr, value) {
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            arr.splice(i, 1);
             i--;
         }
     }
@@ -14,12 +15,7 @@ function removeElement() {
 removeElement(array, 4);
 console.log(array);
 
-
-
 // const array = [1, 3, 4, 4, 6, 2, 5, 4, 7];
 // removeElement(array, 4);
 // console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
 
-
-// console.log(array);
-// return array;
